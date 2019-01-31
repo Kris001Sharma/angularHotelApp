a7bd0269e72df148c80a9b31e915416060b1b07a
Krishna <kris001.sharma@gmail.com>
2019-01-31 15:24:45 +0530

    Initial Commit, created routing module, added hotel pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { OffersComponent } from './offers/offers.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accomodation', component: AccomodationComponent},
+  {path: 'celebrations', component: CelebrationsComponent},
+  {path: 'food-beverages', component: FoodBeverageComponent},
+  {path: 'lifestyle', component: LifestyleComponent},
+  {path: 'offers', component: OffersComponent},
+  {path: 'weddings', component: WeddingsComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
